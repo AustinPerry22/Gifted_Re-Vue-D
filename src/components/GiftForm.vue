98c66bc3b2aae9db9c95c2434031caae127639f2
Austin <austinjperry22@gmail.com>
2023-09-12 22:07:00 -0600

    fixed some bugs

--- src/components/GiftForm.vue
+++ src/components/GiftForm.vue
@@ -1,17 +1,17 @@
 <template>
-    <form @submit.prevent="getGifs" class="row justify-content-center">
+    <form @submit.prevent class="row justify-content-center">
         <div class="col-10 card bg-dark">
-            <label for="giftTag">Gift text</label>
-            <input v-model="newGift.tag" name="giftTag" type="text">
             <label for="giftUrl">Search for Giphy Gif</label>
             <input v-model="searchGif" name="searchGif" type="text">
+            <button @click="getGifs" v-if="searchGif" class="btn btn-success my-2">Search for Gif</button>
             <section class="row">
                 <div @click="newGift.url = gif.url" v-for="gif in   gifs  " :key="gif.id"
                     class="col-3 card img-fluid text-center g-2" :class="{ 'bg-success': newGift.url == gif.url }">
                     <img :src="gif.url" />
                 </div>
             </section>
-            <button v-if="searchGif" class="btn btn-success my-2">Search for Gif</button>
+            <label for="giftTag">Gift text</label>
+            <input v-model="newGift.tag" name="giftTag" type="text">
             <button @click="createGift" v-if="newGift.url" class="btn btn-success">Post Gift</button>
         </div>
     </form>
@@ -46,6 +46,7 @@ export default {
 
             async getGifs() {
                 try {
+                    this.newGift.url = null
                     await gifsService.getGifs(this.searchGif)
                 } catch (error) {
                     Pop.error(error)
